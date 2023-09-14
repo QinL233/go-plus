160bb6f3cdf57f952a4f59610a4cb489c9f3fa6e
liaoqinzhou_sz <liaoqinzhou@sz.baidu.com>
2023-09-14 10:53:59 +0800

    优化gin-server tool

--- web/controller.go
+++ web/controller.go
@@ -1,11 +1,7 @@
 package web
 
 import (
-	"github.com/QinL233/go-plus/orm/mysql"
 	"github.com/gin-gonic/gin"
-	"github.com/pkg/errors"
-	"gorm.io/gorm"
-	"log"
 )
 
 /**
@@ -22,102 +18,4 @@ func Controller(list ...func(g *gin.RouterGroup)) {
 	for _, controller := range list {
 		controllers = append(controllers, controller)
 	}
-}
-
-/**
-Service - 工具
-1、用于快速从context实现做提取param并校验
-2、封装service.func(param)(result,err)方法的返回
-*/
-
-func Service[P any, R any](c *gin.Context, f func(P) (R, error)) {
-	var param P
-	//1、尝试从header中取得参数【不保证校验】 - 获取`header:"paramName"`
-	c.BindHeader(&param)
-	//2、根据参数性质bind参数
-	if c.Params != nil && len(c.Params) > 0 {
-		//uri请求 - 必须注意service使用`uri:"paramName"`接收
-		if err := c.BindUri(&param); err != nil {
-			log.Println(err)
-			fail(c, 500, errors.New("param is fail"))
-			return
-		}
-	} else {
-		//query/json/form请求
-		if err := c.Bind(&param); err != nil {
-			log.Println(err)
-			fail(c, 500, errors.New("param is fail"))
-			return
-		}
-	}
-	//3、回调方法
-	r, err := f(param)
-	if err != nil {
-		fail(c, 500, err)
-		return
-	}
-	//4、封装返回
-	success(c, r)
-}
-
-/**
-从controller层获取driver，以统一会话期间的driver以方便使用事务
-*/
-
-func DBService[P any, R any](c *gin.Context, f func(db *gorm.DB, param P) (R, error)) {
-	var param P
-	//1、尝试从header中取得参数【不保证校验】 - 获取`header:"paramName"`
-	c.BindHeader(&param)
-	//2、根据参数性质bind参数
-	if c.Params != nil && len(c.Params) > 0 {
-		//uri请求 - 必须注意service使用`uri:"paramName"`接收
-		if err := c.BindUri(&param); err != nil {
-			log.Println(err)
-			fail(c, 500, errors.New("param is fail"))
-			return
-		}
-	} else {
-		//query/json/form请求
-		if err := c.Bind(&param); err != nil {
-			log.Println(err)
-			fail(c, 500, errors.New("param is fail"))
-			return
-		}
-	}
-	//3、回调方法
-	r, err := f(mysql.Driver(), param)
-	if err != nil {
-		fail(c, 500, err)
-		return
-	}
-	//4、封装返回
-	success(c, r)
-}
-
-/**
-封装标准的返回格式
-*/
-
-type JSON struct {
-	Code int    `json:"code"`
-	Msg  string `json:"msg"`
-	Data any    `json:"data"`
-}
-
-func success(c *gin.Context, data any) {
-	c.JSON(200, JSON{
-		Code: 200,
-		Msg:  "success",
-		Data: data,
-	})
-	return
-}
-
-func fail(c *gin.Context, code int, err error) {
-	c.JSON(200, JSON{
-		Code: code,
-		Msg:  err.Error(),
-		Data: nil,
-	})
-	return
 }
