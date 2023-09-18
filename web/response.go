a3cc00839702c7c63b0d6db6d0e4b1b1bddfcfc4
liaoqinzhou_sz <liaoqinzhou@sz.baidu.com>
2023-09-18 13:46:02 +0800

    优化response

--- web/response.go
+++ web/response.go
@@ -2,6 +2,7 @@ package web
 
 import (
 	"github.com/gin-gonic/gin"
+	"reflect"
 )
 
 /**
@@ -18,11 +19,7 @@ func Success(c *gin.Context, data any) {
 	if hadWriter(c) {
 		return
 	}
-	c.JSON(200, Response{
-		Code: 200,
-		Msg:  "success",
-		Data: data,
-	})
+	c.JSON(200, parserResponse(data))
 	return
 }
 
@@ -38,15 +35,28 @@ func Fail(c *gin.Context, code int, err error) {
 	return
 }
 
-func Json(c *gin.Context, data any) {
-	if hadWriter(c) {
-		return
-	}
-	c.JSON(200, data)
-	return
-}
-
 //如果response已有东西则不再重复写
 func hadWriter(c *gin.Context) bool {
 	return c.Writer.Size() > 0
+}
+
+//判断是否需要包装json
+func parserResponse(data any) any {
+	t := reflect.TypeOf(data)
+	if t == nil {
+		return Response{200, "success", nil}
+	}
+	_, d1 := t.FieldByName("Code")
+	if !d1 {
+		return Response{200, "success", data}
+	}
+	_, d2 := t.FieldByName("Msg")
+	if !d2 {
+		return Response{200, "success", data}
+	}
+	_, d3 := t.FieldByName("Data")
+	if !d3 {
+		return Response{200, "success", data}
+	}
+	return data
 }
