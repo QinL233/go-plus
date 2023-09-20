c5994ae32e6cde07d427b9eba0f452b6d88e2e2e
liaoqinzhou_sz <liaoqinzhou@sz.baidu.com>
2023-09-20 14:20:09 +0800

    封装create和save dao工具

---
+++ orm/mysql/dao/Save.go
@@ -0,0 +1,11 @@
+package dao
+
+import "gorm.io/gorm"
+
+func Save(db *gorm.DB, entity any) int64 {
+	r := db.Save(&entity)
+	if r.Error != nil {
+		panic(r.Error)
+	}
+	return r.RowsAffected
+}
---
+++ orm/mysql/dao/create.go
@@ -0,0 +1,19 @@
+package dao
+
+import "gorm.io/gorm"
+
+func Create(db *gorm.DB, entity any) int64 {
+	r := db.Create(&entity)
+	if r.Error != nil {
+		panic(r.Error)
+	}
+	return r.RowsAffected
+}
+
+func CreateBatch(db *gorm.DB, entity any, size int) int64 {
+	r := db.CreateInBatches(&entity, size)
+	if r.Error != nil {
+		panic(r.Error)
+	}
+	return r.RowsAffected
+}
